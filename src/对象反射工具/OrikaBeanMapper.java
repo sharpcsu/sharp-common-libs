e31d3ded19c117da9be258e7f01002cd4dfcdb2e
sharpyangwei <sharpyangwei@didiglobal.com>
2021-06-20 12:12:59 +0800

    项目初始化

---
+++ src/对象反射工具/OrikaBeanMapper.java
@@ -0,0 +1,39 @@
+package 对象反射工具;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+
+/**
+ * Create by @author sharpcsu on 2021/6/20 - 12:03
+ */
+public class OrikaBeanMapper {
+
+    private static final MapperFactory FACTORY;
+
+    static {
+        FACTORY = new DefaultMapperFactory.Builder().build();
+    }
+
+    public static <V, P> P convert(V base, final Class<P> target) {
+        if (base == null) {
+            return null;
+        }
+
+        return FACTORY.getMapperFacade().map(base, target);
+    }
+
+    public static <V, P> List<P> convertList(List<V> baseList, Class<P> target) {
+        if (baseList == null) {
+            return null;
+        }
+
+        List<P> targetList = new ArrayList<>();
+        for (V vo : baseList) {
+            targetList.add(convert(vo, target));
+        }
+        return targetList;
+    }
+}
